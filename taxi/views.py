6b58b7aacadb072fe073072e96a0ec9096ee2e22
Andrii <yurkivandriy02@gmail.com>
2025-04-08 00:09:21 +0300

    fix flake8

--- taxi/views.py
+++ taxi/views.py
@@ -46,7 +46,9 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ManufacturerListView, self).get_context_data(**kwargs)
         name = self.request.GET.get("name", "")
-        context["search_form"] = ManufacturerNameSearchForm(initial={"name": name})
+        context["search_form"] = ManufacturerNameSearchForm(
+            initial={"name": name}
+        )
         return context
 
     def get_queryset(self):
@@ -120,7 +122,9 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(DriverListView, self).get_context_data(**kwargs)
         username = self.request.GET.get("username", "")
-        context["search_form"] = DriverUsernameSearchForm(initial={"username": username})
+        context["search_form"] = DriverUsernameSearchForm(
+            initial={"username": username}
+        )
         return context
 
     def get_queryset(self):
